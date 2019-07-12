6a729519afb4d0382c222c99916c5d9a55897c3b
Ramarao <yadlapalli9999@gmail.com>
2019-07-12 21:37:50 +0530

    completed ecommerce application

--- assignment/task-22/main.js
+++ assignment/task-22/main.js
@@ -1,9 +1,8 @@
 function dataList(){
     document.getElementById('demo').innerHTML = ""
     for(k=0;k<data.length;k++){
-        console.log(data[k])
+        //console.log(data[k])
         var mainDiv = document.createElement('div')
-
         mainDiv.innerHTML = '<div class="card" style="width: 18rem;text-align:center"><img style="width:200px;height:200px;" src="'+data[k].image+'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+data[k].name+'</h5><p class="card-text">'+data[k].cost+'</p><a type="button" class="btn btn-primary" onclick="addCart('+k+')">addCart</a></div></div>'
         mainDiv.setAttribute('style',"float:left;margin-left:20px;margin-top:10px;")
 
@@ -13,18 +12,10 @@ function dataList(){
 dataList()
 
 function All(){
-    document.getElementById('demo').innerHTML = ""
+    dataList()
     for(k=0;k<data.length;k++){
-       
-         var mainDiv = document.createElement('div')
- 
-         mainDiv.innerHTML = '<div class="card" style="width: 18rem;text-align:center"><img style="width:200px;height:200px;" src="'+data[k].image+'" class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">'+data[k].name+'</h5><p class="card-text">'+data[k].cost+'</p><a type="button" class="btn btn-primary" onclick="addCart('+k+')">addCart</a></div></div>'
-         mainDiv.setAttribute('style',"float:left;margin-left:20px;margin-top:10px;")
- 
-         document.getElementById('demo').appendChild(mainDiv)
         document.getElementById('all-item').innerHTML = data.length
-        }
-     
+    }
 }
 All()
 
@@ -88,10 +79,33 @@ function Shirts(){
 }
 Shirts()
 
-function addCart(i){
 
+var cartItem = []
+cartItem =JSON.parse(localStorage.getItem('item_key1'))
 
+var count = 0;
+function addCart(index){
+    if(itcartItem==null){
+             cartItem =JSON.parse(localStorage.getItem('item_key1'))
+            }else{
+                index = parseInt(index);
+  cartItem.push(data[index]);
+          }
+// console.log(index)    
+//  index = parseInt(index);
+//   cartItem.push(data[index]);
+  
+  
+  document.getElementById('collect').innerHTML = cartItem.length;
 }
-addCart()
 
-
+function cart(){
+   console.log(cartItem)
+//    for(m =0; m<cartItem.length;m++){
+      
+//    }
+localStorage.setItem("item_key", JSON.stringify(cartItem))
+   window.location.href ="./cart-1.html"
+  
+  
+}
